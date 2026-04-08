86ef0b4092875c35856cdab79264437b0c0047e3
montserodriguez <rodrmontserrat4@gmail.com>
2026-04-08 15:52:52 -0500

    cálculo dependencia económica

--- scripts/economia-y-empleo.R
+++ scripts/economia-y-empleo.R
@@ -20,8 +20,13 @@ censo_2020 <- tbl(src = implan,
                       table = 'censo_2020'))
 
 manzanas <- tbl(siginplan,
-                 Id(schema = '00_base',
-                    table = 'manzanas_codigos'))
+                Id(schema = '00_base',
+                   table = 'manzanas_codigos'))
+
+iter_nal <- read_csv('../procesamiento-coati/datos/iter/iter_nal2020.csv') %>% 
+  rename_with(tolower) %>% 
+  mutate(across(c(longitud:last_col()),
+                as.numeric))
 # Funciones ----
 
 
@@ -32,7 +37,23 @@ manzanas <- tbl(siginplan,
 # Dependencia económica 
 # (personas 0 a 12 años + viejitos / PEA)
 
-iter %>% 
-  filter(nom_ent == '23' & nom_mun == '005' & nom_loc == 'Total municipio') %>% 
-  summarise(idk = p_0a2 + p)
+dependencia_economica <- iter_nal %>%
+  filter(entidad == '23',
+         mun == '005',
+         nom_loc == 'Total del Municipio') %>%
+  summarise(dependientes = p_0a2 + p_3a5 + p_6a11 + p_12a14 + p_60ymas,
+            independientes = pea,
+            dependencia = (dependientes) / independientes * 100) %>% 
+  pivot_longer(c(dependientes, independientes, dependencia),
+               names_to = 'situacion_economica',
+               values_to = 'cantidad') %>% 
+  mutate(porcentajes = ifelse(situacion_economica == 'dependencia', 
+                              NA,
+                              round (cantidad / sum(cantidad)*100, 1)))
+
+dbWriteTable(implan,
+             Id (schema = 'coati_tablas_finales',
+                 table = 'g_dependencia_economica'),
+             dependencia_economica,
+             overwrite = T)
 
